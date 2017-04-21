880aad54751f87be5d3b02f5c93de71af61c4407
leandrocgsi <leandrocgsi@gmail.com>
2017-04-20 21:39:06 -0300

    Hello ASP.NET

---
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using restful_api_with_aspnet.Models;
+
+namespace restful_api_with_aspnet
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
