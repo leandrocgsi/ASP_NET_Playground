f5179a8c631450d2067586c072322797c568a00b
leandrocgsi <leandrocgsi@gmail.com>
2017-04-21 21:40:49 -0300

    Hello ASP.NET

---
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Repository/IContactsRepository.cs
@@ -0,0 +1,14 @@
+﻿using restful_api_with_aspnet.Models;
+using System.Collections.Generic;
+
+namespace restful_api_with_aspnet.Repository
+{
+    public interface IContactsRepository
+{
+    void Add(Contacts item);
+    IEnumerable<Contacts> GetAll();
+    Contacts Find(string key);
+    void Remove(string Id);
+    void Update(Contacts item);
+}
+}
