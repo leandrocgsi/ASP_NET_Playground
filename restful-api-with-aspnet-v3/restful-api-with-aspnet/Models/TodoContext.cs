4aa13bb7f7c7f1a7bad489b3591f92f592c05c2e
leandrocgsi <leandrocgsi@gmail.com>
2017-04-26 21:43:39 -0300

    Hello ASP.NET

---
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace restful_api_with_aspnet.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
