5ddb95a6e94af0f0eb607de6fb5e6dbc84b0e9cf
leandrocgsi <leandrocgsi@gmail.com>
2017-04-28 21:44:26 -0300

    Hello ASP.NET

---
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace restful_api_with_aspnet.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
