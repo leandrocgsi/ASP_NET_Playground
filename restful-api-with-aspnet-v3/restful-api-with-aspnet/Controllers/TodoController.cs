be9380f89792eacab9ffc6a9847e9465f3ab323f
leandrocgsi <leandrocgsi@gmail.com>
2017-04-29 21:46:12 -0300

    Hello ASP.NET

---
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using restful_api_with_aspnet.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace restful_api_with_aspnet.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+
+        private readonly TodoContext _context;
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
