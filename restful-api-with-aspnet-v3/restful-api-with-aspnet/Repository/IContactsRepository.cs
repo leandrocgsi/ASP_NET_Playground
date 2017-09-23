3221fea2b77064b88eb43a3e6b087f242475d53e
leandrocgsi <leandrocgsi@gmail.com>
2017-09-23 11:29:57 -0300

    Quick refactory

--- restful-api-with-aspnet-v3/restful-api-with-aspnet/Repository/IContactsRepository.cs
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Repository/IContactsRepository.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 namespace restful_api_with_aspnet.Repository
 {
     public interface IContactsRepository
-{
-    void Add(Contacts item);
-    IEnumerable<Contacts> GetAll();
-    Contacts Find(string key);
-    void Remove(string Id);
-    void Update(Contacts item);
-}
+    {
+        void Add(Contacts item);
+        IEnumerable<Contacts> GetAll();
+        Contacts Find(string key);
+        void Remove(string Id);
+        void Update(Contacts item);
+    }
 }
