2c9d6f1ae5fbf12c7b6754e3a71b54c6b55b18a9
leandrocgsi <leandrocgsi@gmail.com>
2017-09-24 12:27:06 -0300

    Hello ASP.NET

--- restful-api-with-aspnet-v3/restful-api-with-aspnet/Controllers/TodoController.cs
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Controllers/TodoController.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 
 using restful_api_with_aspnet.Models;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace restful_api_with_aspnet.Controllers
 {
     [Route("api/[controller]")]
