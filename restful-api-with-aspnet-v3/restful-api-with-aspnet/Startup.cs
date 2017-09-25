6dbd693bedd401b7a869e513662d4fe3afad93ab
Leandro Costa <leandrocgsi@gmail.com>
2017-09-25 11:02:10 -0300

    Fixing bug in ContactsEndpoint

--- restful-api-with-aspnet-v3/restful-api-with-aspnet/Startup.cs
+++ restful-api-with-aspnet-v3/restful-api-with-aspnet/Startup.cs
@@ -1,18 +1,28 @@
-﻿using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using restful_api_with_aspnet.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Serialization;
+
+using restful_api_with_aspnet.Repository;
 
 namespace restful_api_with_aspnet
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(a => a.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+            services.AddSingleton<IContactsRepository, ContactsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
