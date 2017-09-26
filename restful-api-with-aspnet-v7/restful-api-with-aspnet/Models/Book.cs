d3f77751a4a9d6678423a646e66c098f84780d2a
leandrocgsi <leandrocgsi@gmail.com>
2017-09-26 01:18:37 -0300

    Connecting with MySQL. But still not working

---
+++ restful-api-with-aspnet-v7/restful-api-with-aspnet/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace restful_api_with_aspnet.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ restful-api-with-aspnet-v7/restful-api-with-aspnet/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
+
+using restful_api_with_aspnet.Repository;
+
+namespace restful_api_with_aspnet
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(a => a.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
+            //services.AddDbContext<ContactsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
+            //using Dependency Injection
+            services.AddSingleton<IBookRepository, BookRepository>();
+            services.AddSingleton<IContactsRepository, ContactsRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
