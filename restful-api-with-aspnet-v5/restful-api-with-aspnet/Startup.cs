fa52d18f47ee60dae15cdbd2916b49a997b4ef35
leandrocgsi <leandrocgsi@gmail.com>
2017-09-26 01:15:16 -0300

    Creating a new endpoint

---
+++ restful-api-with-aspnet-v5/restful-api-with-aspnet/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
+using restful_api_with_aspnet.Repository;
+
+namespace restful_api_with_aspnet
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(a => a.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
+            //services.AddDbContext<ContactsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            //using Dependency Injection
+            services.AddSingleton<IBookRepository, BookRepository>();
+            services.AddSingleton<IContactsRepository, ContactsRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
