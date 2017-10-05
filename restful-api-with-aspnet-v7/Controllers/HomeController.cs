42a82fae0e696150924cd607b35291583e06ca9e
leandrocgsi <leandrocgsi@gmail.com>
2017-10-05 12:52:43 -0300

    Trying redirect to Swagger page

---
+++ restful-api-with-aspnet-v7/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace restful_api_with_aspnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new RedirectResult("~/swagger/ui/index");
+        }
+    }
+}
