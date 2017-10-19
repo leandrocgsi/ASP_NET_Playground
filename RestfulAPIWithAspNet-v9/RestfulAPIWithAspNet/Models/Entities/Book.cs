3650614276563faecc9b2885c6cbd97d2167088d
leandrocgsi <leandrocgsi@gmail.com>
2017-10-19 17:38:00 -0200

    Setting serialize order

--- RestfulAPIWithAspNet-v9/RestfulAPIWithAspNet/Models/Entities/Book.cs
+++ RestfulAPIWithAspNet-v9/RestfulAPIWithAspNet/Models/Entities/Book.cs
@@ -1,14 +1,23 @@
 ﻿using RestfulAPIWithAspNet.Models.Entities.Base;
 using System;
+using System.Runtime.Serialization;
 
 namespace RestfulAPIWithAspNet.Models.Entities
 {
+    [DataContract]
     public class Book : BaseEntity
     {
 
+        [DataMember(Order = 2)]
         public string Title { get; set; }
+
+        [DataMember(Order = 3)]
         public string Author { get; set; }
+
+        [DataMember(Order = 4)]
         public decimal Price { get; set; }
+
+        [DataMember(Order = 5)]
         public DateTime LaunchDate { get; set; } = DateTime.UtcNow;
     }
 }
