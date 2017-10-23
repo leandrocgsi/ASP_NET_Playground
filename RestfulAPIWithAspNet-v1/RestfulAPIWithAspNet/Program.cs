6a246aa6376a9985bba4f7c6a6411e757876e95c
leandrocgsi <leandrocgsi@gmail.com>
2017-10-23 00:02:44 -0200

    Quick refactory

--- RestfulAPIWithAspNet-v1/RestfulAPIWithAspNet/Program.cs
+++ RestfulAPIWithAspNet-v1/RestfulAPIWithAspNet/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace restful_api_with_aspnet
+namespace RestfulAPIWithAspNet
 {
     public class Program
     {
--- RestfulAPIWithAspNet-v1/RestfulAPIWithAspNet/Startup.cs
+++ RestfulAPIWithAspNet-v1/RestfulAPIWithAspNet/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace restful_api_with_aspnet
+namespace RestfulAPIWithAspNet
 {
     public class Startup
     {
