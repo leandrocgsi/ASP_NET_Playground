539d043dd92d190766d92a59ee933a1b699567ac
leandrocgsi <leandrocgsi@gmail.com>
2017-10-26 12:58:34 -0200

    Adding VO's

---
+++ RestfulAPIWithAspNet-v10/RestfulAPIWithAspNet/Data/VO/BookVO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RestfulAPIWithAspNet.Data.VO
+{
+    public class BookVO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LaunchDate { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ RestfulAPIWithAspNet-v10/RestfulAPIWithAspNet/Data/VO/PersonVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RestfulAPIWithAspNet.Data.VO
+{
+    public class PersonVO
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public String Cpf { get; set; }
+        public DateTime BirthDayDate { get; set; }
+        public String Login { get; set; }
+        public String Password { get; set; }
+        public String Permission { get; set; }
+    }
+}
