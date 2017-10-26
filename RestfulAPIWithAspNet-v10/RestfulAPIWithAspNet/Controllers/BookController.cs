60a31517d69a6b61f335046d17bd93809366b293
leandrocgsi <leandrocgsi@gmail.com>
2017-10-26 12:50:04 -0200

    Adding VO's

---
+++ RestfulAPIWithAspNet-v10/RestfulAPIWithAspNet/Controllers/BookController.cs
@@ -0,0 +1,83 @@
+﻿using RestfulAPIWithAspNet.Repository;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestfulAPIWithAspNet.Data.DTO;
+using RestfulAPIWithAspNet.Models.Entities;
+using RestfulAPIWithAspNet.Utils.Data;
+
+namespace RestfulAPIWithAspNet.Controllers
+{
+    //HACK: See https://www.packtpub.com/application-development/restful-web-services-aspnet-core-video
+    //SEE: https://github.com/bragil/diario-bordo and https://bragil.wordpress.com/2012/12/13/dao-generico-entity-framework-5-code-first/
+
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+
+        private readonly ILogger _logger;
+
+        private IRepository<Book> _bookRepository;
+        QueryBuilder<Book> queryBuilder = new QueryBuilder<Book>();
+
+        public BookController(IRepository<Book> repository, ILogger<BookController> logger)
+        {
+            _bookRepository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetAllAsync()
+        {
+            return _bookRepository.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetBook")]
+        public IActionResult GetByIdAsync(string id)
+        {
+            if (id == null || "".Equals(id)) return BadRequest();
+            var book = _bookRepository.Find(id);
+            if (book == null) return this.NotFound();
+            return this.Ok(book);
+        }
+
+        [HttpPost("PagedSearch")]
+        public IActionResult PagedSearch([FromBody] PagedSearchDTO<Book> pagedSearchDTO)
+        {
+            string query = queryBuilder.WithDTO(pagedSearchDTO).GetQueryFromDTO("b", "books");
+          
+            pagedSearchDTO.List = _bookRepository.FindWithPagedSearch(query);
+            pagedSearchDTO.TotalResults = _bookRepository.GetCount(queryBuilder.WithDTO(pagedSearchDTO).GetBaseSelectCount("b", "books"));
+
+            return new ObjectResult(pagedSearchDTO);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Book book)
+        {
+            if (book == null) return BadRequest();
+            var returnBook = _bookRepository.Add(book);
+            return new ObjectResult(returnBook);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody]Book book)
+        {
+            var returnBook = new Book();
+            var result = _bookRepository.Exists(book.Id);
+            if (!result) return this.BadRequest();
+            _bookRepository.Update(book);
+            return new ObjectResult(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            if (id == null || "".Equals(id)) return BadRequest();
+            var result = _bookRepository.Exists(id);
+            if (!result) return NotFound();
+            _bookRepository.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ RestfulAPIWithAspNet-v10/RestfulAPIWithAspNet/Controllers/FilmController.cs
@@ -0,0 +1,80 @@
+﻿using RestfulAPIWithAspNet.Repository;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestfulAPIWithAspNet.Data.DTO;
+using RestfulAPIWithAspNet.Models.Entities;
+using RestfulAPIWithAspNet.Utils.Data;
+
+namespace RestfulAPIWithAspNet.Controllers
+{
+    [Route("api/[controller]")]
+    public class FilmController : ControllerBase
+    {
+
+        private readonly ILogger _logger;
+
+        private IRepository<Film> _FilmRepository;
+        QueryBuilder<Film> queryBuilder = new QueryBuilder<Film>();
+
+        public FilmController(IRepository<Film> repository, ILogger<FilmController> logger)
+        {
+            _FilmRepository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Film> GetAllAsync()
+        {
+            return _FilmRepository.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetFilm")]
+        public IActionResult GetByIdAsync(string id)
+        {
+            if (id == null || "".Equals(id)) return BadRequest();
+            var film = _FilmRepository.Find(id);
+            if (film == null) return this.NotFound();
+            return this.Ok(film);
+        }
+
+        [HttpPost("PagedSearch")]
+        public IActionResult PagedSearch([FromBody] PagedSearchDTO<Film> pagedSearchDTO)
+        {
+            string query = queryBuilder.WithDTO(pagedSearchDTO).GetQueryFromDTO("f", "films");
+
+            pagedSearchDTO.List = _FilmRepository.FindWithPagedSearch(query);
+            pagedSearchDTO.TotalResults = _FilmRepository.GetCount(queryBuilder.WithDTO(pagedSearchDTO).GetBaseSelectCount("f", "films"));
+
+            return new ObjectResult(pagedSearchDTO);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Film film)
+        {
+            if (film == null) return BadRequest();
+            var returnFilm = _FilmRepository.Add(film);
+            return new ObjectResult(returnFilm);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody]Film film)
+        {
+            var returnFilm = new Film();
+            var result = _FilmRepository.Exists(film.Id);
+            if (!result) return this.BadRequest();
+            _FilmRepository.Update(film);
+            return new ObjectResult(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            if (id == null || "".Equals(id)) return BadRequest();
+            var result = _FilmRepository.Exists(id);
+            if (!result) return NotFound();
+            _FilmRepository.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
