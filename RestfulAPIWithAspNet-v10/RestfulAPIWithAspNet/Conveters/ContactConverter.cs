72c50d8e7c5e8f04255425c67f78be4c08be0b6f
leandrocgsi <leandrocgsi@gmail.com>
2017-10-27 10:18:52 -0200

    Adding converter to ContactConverter

---
+++ RestfulAPIWithAspNet-v10/RestfulAPIWithAspNet/Conveters/ContactConverter.cs
@@ -0,0 +1,51 @@
+﻿using RestfulAPIWithAspNet.Data.VO;
+using RestfulAPIWithAspNet.Models.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using UpBrasil.OTP.API.Utils;
+
+namespace RestfulAPIWithAspNet.Conveters
+{
+    public class ContactConverter : IParser<Contact, ContactVO>, IParser<ContactVO, Contact>
+    {
+        public Contact Parse(ContactVO origin)
+        {
+            if (origin == null) return new Contact();
+            return new Contact
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                AnniversaryDate = origin.AnniversaryDate,
+                DateOfBirth = origin.DateOfBirth
+            };
+        }
+
+        public ContactVO Parse(Contact origin)
+        {
+            if (origin == null) return new ContactVO();
+            return new ContactVO
+            {
+                Id = origin.Id,
+            };
+        }
+
+        internal List<Contact> ParseVOListToEntityList(List<ContactVO> Contacts)
+        {
+            if (Contacts == null) return new List<Contact>();
+            return Contacts.Select(item => Parse(item)).ToList();
+        }
+
+        internal List<ContactVO> ParseEntityListToVOList(List<Contact> Contacts)
+        {
+            if (Contacts == null) return new List<ContactVO>();
+            return Contacts.Select(item => Parse(item)).ToList();
+        }
+
+        internal IEnumerable<ContactVO> ParseEntityListToVOList(IEnumerable<Contact> Contacts)
+        {
+            if (Contacts == null) return new List<ContactVO>();
+            return Contacts.Select(item => Parse(item)).ToList();
+        }
+    }
+}
