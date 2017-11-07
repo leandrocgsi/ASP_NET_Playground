c8283f7bfdc17ab4a3e7734ca41b205d99261f3c
leandrocgsi <leandrocgsi@gmail.com>
2017-11-07 13:54:37 -0200

    Refactoring to split loging between Controller and Business

--- RestfulAPIWithAspNet-v11/RestfulAPIWithAspNet/Startup.cs
+++ RestfulAPIWithAspNet-v11/RestfulAPIWithAspNet/Startup.cs
@@ -49,6 +49,8 @@ namespace RestfulAPIWithAspNet
             });
 
             services.AddScoped<BookBusiness>();
+            services.AddScoped<ContactBusiness>();
+            services.AddScoped<FilmBusiness>();
 
             //services.AddScoped<IRepository<Book>, GenericRepository<Book>>();
             services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
