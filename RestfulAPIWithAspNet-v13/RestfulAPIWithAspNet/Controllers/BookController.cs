6eb1019536e8f5cb2bd345e619d87544ba1e8c7a
leandrocgsi <leandrocgsi@gmail.com>
2017-11-27 09:21:57 -0200

    Implementing internationalization I18N

---
+++ RestfulAPIWithAspNet-v13/RestfulAPIWithAspNet/Controllers/BookController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestfulAPIWithAspNet.Data.DTO;
+using RestfulAPIWithAspNet.Models.Entities;
+using RestfulAPIWithAspNet.Data.VO;
+using RestfulAPIWithAspNet.Business;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace RestfulAPIWithAspNet.Controllers
+{
+    //HACK: See https://www.packtpub.com/application-development/restful-web-services-aspnet-core-video
+    
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+
+        private BookBusiness _business;
+
+        public BookController(BookBusiness business)
+        {
+            _business = business;
+        }
+
+        [HttpGet]
+        [SwaggerResponse((200), Type = typeof(List<BookVO>))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        public IEnumerable<BookVO> GetAll()
+        {
+            return _business.FindAll();
+        }
+
+        [HttpGet("{id}")]
+        [SwaggerResponse((200), Type = typeof(BookVO))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        public IActionResult GetById(string id)
+        {
+            if (id == null || "".Equals(id)) return BadRequest();
+            var item = _business.GetByIdAsync(id);
+            if (item == null) return this.NotFound();
+            return this.Ok(item);
+        }
+
+        [HttpPost("PagedSearch")]
+        [SwaggerResponse((200), Type = typeof(PagedSearchDTO<Book>))]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        public IActionResult PagedSearch([FromBody] PagedSearchDTO<Book> pagedSearchDTO)
+        {
+            return new ObjectResult(_business.PagedSearch(pagedSearchDTO));
+        }
+
+        [HttpPost]
+        [SwaggerResponse((201), Type = typeof(BookVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        public IActionResult Create([FromBody]Book item)
+        {
+            if (item == null) return BadRequest();
+            return new ObjectResult(_business.Create(item));
+        }
+
+        [HttpPut]
+        [SwaggerResponse((202), Type = typeof(BookVO))]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        public IActionResult Update([FromBody]Book item)
+        {
+            var updated = _business.Update(item);
+            if (updated.Id == null) return this.BadRequest();
+            return new ObjectResult(_business.Update(item));
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerResponse(204)]
+        [SwaggerResponse(400)]
+        [SwaggerResponse(401)]
+        public IActionResult Delete(string id)
+        {
+            if (id == null || "".Equals(id)) return BadRequest();
+            if (!_business.Delete(id)) return NotFound();
+            return new NoContentResult();
+        }
+    }
+}
---
+++ RestfulAPIWithAspNet-v13/RestfulAPIWithAspNet/Conveters/FilmConverter.cs
@@ -0,0 +1,57 @@
+﻿using RestfulAPIWithAspNet.Data.VO;
+using RestfulAPIWithAspNet.Models.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using UpBrasil.OTP.API.Utils;
+
+namespace RestfulAPIWithAspNet.Conveters
+{
+    public class FilmConverter : IParser<Film, FilmVO>, IParser<FilmVO, Film>
+    {
+        public Film Parse(FilmVO origin)
+        {
+            if (origin == null) return new Film();
+            return new Film
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Description = origin.Description,
+                ReleaseYear = origin.ReleaseYear,
+                Length = origin.Length,
+                Rating = origin.Rating
+            };
+        }
+
+        public FilmVO Parse(Film origin)
+        {
+            if (origin == null) return new FilmVO();
+            return new FilmVO
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Description = origin.Description,
+                ReleaseYear = origin.ReleaseYear,
+                Length = origin.Length,
+                Rating = origin.Rating
+            };
+        }
+
+        internal List<Film> ParseVOListToEntityList(List<FilmVO> Films)
+        {
+            if (Films == null) return new List<Film>();
+            return Films.Select(item => Parse(item)).ToList();
+        }
+
+        internal List<FilmVO> ParseEntityListToVOList(List<Film> Films)
+        {
+            if (Films == null) return new List<FilmVO>();
+            return Films.Select(item => Parse(item)).ToList();
+        }
+
+        internal IEnumerable<FilmVO> ParseEntityListToVOList(IEnumerable<Film> Films)
+        {
+            if (Films == null) return new List<FilmVO>();
+            return Films.Select(item => Parse(item)).ToList();
+        }
+    }
+}
