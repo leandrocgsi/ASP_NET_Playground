8e991c601639de2500bc0f02c157099f1f7cd6d6
leandrocgsi <leandrocgsi@gmail.com>
2017-11-28 09:21:08 -0200

    Refactoring to I18N

---
+++ RestfulAPIWithAspNet-v13/RestfulAPIWithAspNet/Utils/I18NMessageHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAPIWithAspNet.Utils
+{
+    public class I18NMessageHandler
+    {
+    }
+}
