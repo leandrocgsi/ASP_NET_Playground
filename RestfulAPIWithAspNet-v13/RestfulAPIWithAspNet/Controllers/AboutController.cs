452aa721661e48bf03226fec3958f28049e56c25
leandrocgsi <leandrocgsi@gmail.com>
2017-11-29 10:52:43 -0200

    Refactoring to I18N

---
+++ RestfulAPIWithAspNet-v13/RestfulAPIWithAspNet/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
+using I18N;
+
+namespace RestfulAPIWithAspNet.Controllers
+{
+    [Route("api/[controller]")]
+    public class AboutController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return Resource.WELCOME;
+        }
+    }
+}
