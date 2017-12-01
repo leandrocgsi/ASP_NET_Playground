547f6c3edca1b6661340c11d8066c89f35f4b11c
leandrocgsi <leandrocgsi@gmail.com>
2017-12-01 09:42:07 -0200

    Implementing HATEOAS support

--- RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Controllers/AboutController.cs
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Controllers/AboutController.cs
@@ -7,6 +7,11 @@ using I18N;
 
 namespace RestfulAPIWithAspNet.Controllers
 {
+    // SEE: HATEOAS https://shatzisblog.wordpress.com/2017/09/01/generating-hypermedia-links-for-an-asp-net-core-web-api/ 
+    // and https://github.com/SotirisH/HyperMedia
+    // and http://pracujlabs.io/2015/04/30/becoming-hateoas-with-webapi.html
+    // and https://www.codeproject.com/Articles/1204190/ASP-NET-Core-Web-API-and-HATEOAS
+    // and http://benfoster.io/blog/generating-hypermedia-links-in-aspnet-web-api
     [Route("api/[controller]")]
     public class AboutController : Controller
     {
