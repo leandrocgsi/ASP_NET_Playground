68ae6771f7a32e9963e07d7ace5524489b607ddf
Leandro Costa <leandrocgsi@gmail.com>
2017-12-28 16:14:23 -0200

    Add files via upload

---
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNetTest/UnixTimeTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UpBrasil.OTP.API.Tests.Controllers
+{
+    [TestClass]
+    public class UnixTimeTest
+    {
+      
+
+        [TestMethod]
+        public void TestUnixTime()
+        {
+            Double unixTimeStamp = (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+
+            var d = dtDateTime;
+
+        }
+
+        public DateTime UnixTimeStampToDateTime(Double unixTimeStamp)
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+    }
+}
