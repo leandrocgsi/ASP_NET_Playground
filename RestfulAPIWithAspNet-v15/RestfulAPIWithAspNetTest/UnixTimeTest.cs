3eccdfe0b9df3717a1da8b87c7e0eaa4ea22af6d
Leandro Costa <leandrocgsi@gmail.com>
2017-12-29 09:04:37 -0200

    Update UnixTimeTest.cs

--- RestfulAPIWithAspNet-v15/RestfulAPIWithAspNetTest/UnixTimeTest.cs
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNetTest/UnixTimeTest.cs
@@ -6,13 +6,11 @@ namespace RestfulAPIWithAspNet.Test
     [TestClass]
     public class UnixTimeTest
     {
-      
-
+    
         [TestMethod]
         public void TestUnixTime()
         {
             Double unixTimeStamp = (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-
             DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
 
