205be95856b44eec4f9a93c592019d888bec8ff2
leandrocgsi <leandrocgsi@gmail.com>
2018-01-08 15:17:22 -0200

    Adding HATEOAS support

--- RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Program.cs
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace RestfulAPIWithAspNet
 {
@@ -7,12 +7,15 @@ namespace RestfulAPIWithAspNet
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
+
+            host.Run();
+        }
     }
 }
