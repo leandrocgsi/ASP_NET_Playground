656fc07f80f8c18d1ba6375bd1bf361e2affc6af
leandrocgsi <leandrocgsi@gmail.com>
2018-01-08 13:33:52 -0200

    Adding HATEOAS support

---
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/HyperMedia/ProductEnricher.cs
@@ -0,0 +1,44 @@
+﻿using HATEOAS;
+using Microsoft.AspNetCore.Mvc;
+using RestfulAPIWithAspNet.Data.VO;
+using System.Threading.Tasks;
+
+namespace RestfulAPIWithAspNet.Middleware
+{
+    public class ProductModelEnricher : ObjectContentResponseEnricher<BookVO>
+    {
+        protected override Task EnrichModel(BookVO content, IUrlHelper urlHelper)
+        {
+            //https://blogs.msdn.microsoft.com/roncain/2012/07/17/using-the-asp-net-web-api-urlhelper/
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.GET,
+                Href = urlHelper.Link("DefaultApi", new { controller = "Products", id = content.Id }),
+                Rel = RelationType.self,
+                Type = RensponseTypeFormat.DefaultGet
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.POST,
+                Href = urlHelper.Link("DefaultApi", new { controller = "Products", id = content.Id }),
+                Rel = RelationType.self,
+                Type = RensponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PUT,
+                Href = urlHelper.Link("DefaultApi", new { controller = "Products", id = content.Id }),
+                Rel = RelationType.self,
+                Type = RensponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.DELETE,
+                Href = urlHelper.Link("DefaultApi", new { controller = "Products", id = content.Id }),
+                Rel = RelationType.self,
+                Type = "int"
+            });
+            return null;
+        }
+    }
+}
