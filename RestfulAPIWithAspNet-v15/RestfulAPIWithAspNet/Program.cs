4d1cc9ea0995c5964b325ba777ffa3e95cdc5254
leandrocgsi <leandrocgsi@gmail.com>
2018-01-10 16:03:03 -0200

    Implementing HATEOAS

--- RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Program.cs
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using System.IO;
 
 namespace RestfulAPIWithAspNet
@@ -7,15 +8,12 @@ namespace RestfulAPIWithAspNet
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
