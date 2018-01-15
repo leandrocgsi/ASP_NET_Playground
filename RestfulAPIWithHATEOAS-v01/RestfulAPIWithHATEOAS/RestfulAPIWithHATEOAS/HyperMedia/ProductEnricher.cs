d0c57dc81c24e3824e18436515f31673615f60bf
leandrocgsi <leandrocgsi@gmail.com>
2018-01-15 13:54:59 -0200

    Removing unused imports

--- RestfulAPIWithHATEOAS-v01/RestfulAPIWithHATEOAS/RestfulAPIWithHATEOAS/HyperMedia/ProductEnricher.cs
+++ RestfulAPIWithHATEOAS-v01/RestfulAPIWithHATEOAS/RestfulAPIWithHATEOAS/HyperMedia/ProductEnricher.cs
@@ -1,9 +1,6 @@
 ﻿using HATEOAS;
 using Microsoft.AspNetCore.Mvc;
 using RestfulAPIWithHATEOAS.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestfulAPIWithHATEOAS.HyperMedia
