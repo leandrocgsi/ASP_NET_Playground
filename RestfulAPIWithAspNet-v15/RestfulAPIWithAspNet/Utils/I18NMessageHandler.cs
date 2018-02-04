128a8e53e4d1ee463e29469445c6aa71e6ffcffc
leandrocgsi <leandrocgsi@gmail.com>
2018-02-04 20:49:06 -0200

    Uncomenting code

--- RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Utils/I18NMessageHandler.cs
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Utils/I18NMessageHandler.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -68,4 +68,4 @@ namespace RestfulAPIWithAspNet.Utils
             return response;
         }
     }
-}*/
+}
