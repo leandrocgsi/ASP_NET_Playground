b5bb2354b0e4c3035f693d3692cbcb7e13fd2ff5
leandrocgsi <leandrocgsi@gmail.com>
2018-03-16 09:22:51 -0300

    Changing parser implementations

--- RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Utils/Converter/Parser.cs
+++ RestfulAPIWithAspNet-v15/RestfulAPIWithAspNet/Utils/Converter/Parser.cs
@@ -5,37 +5,6 @@ namespace RestfulAPIWithAspNet.Utils.Converter
     public interface IParser<O, D>
     {
         D Parse(O origin);
-    }
-
-    public abstract class Parser<R>
-    {
-        public Error _Error { get; private set; }
-        public R _Data { get; private set; }
-
-        public bool IsOk
-        {
-            get
-            {
-                return _Error == null;
-            }
-        }
-
-        public bool HasError
-        {
-            get
-            {
-                return !IsOk;
-            }
-        }
-
-        protected Parser(R data)
-        {
-            _Data = data;
-        }
-
-        protected Parser(Error error)
-        {
-            _Error = error;
-        }
+        List<D> ParseList(List<O> origin);
     }
 }
