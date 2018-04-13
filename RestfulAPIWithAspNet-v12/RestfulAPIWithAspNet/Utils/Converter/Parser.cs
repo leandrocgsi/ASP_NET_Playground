0105b53fc73e32257d75d68a09e590069c6991da
leandrocgsi <leandrocgsi@gmail.com>
2018-04-13 09:18:44 -0300

    Quick refactory

--- RestfulAPIWithAspNet-v12/RestfulAPIWithAspNet/Utils/Converter/Parser.cs
+++ RestfulAPIWithAspNet-v12/RestfulAPIWithAspNet/Utils/Converter/Parser.cs
@@ -5,6 +5,6 @@ namespace UpBrasil.OTP.API.Utils
     public interface IParser<O, D>
     {
         D Parse(O origin);
-        /*List<D> ParseList(List<O> origin);*/
+        List<D> ParseList(List<O> origin);
     }
 }
