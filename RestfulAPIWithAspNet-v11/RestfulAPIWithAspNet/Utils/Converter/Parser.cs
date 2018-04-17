b0ba3fbe170c109e8d77d21819a07f69395e27af
leandrocgsi <leandrocgsi@gmail.com>
2018-04-17 09:20:10 -0300

    Quick refactory

--- RestfulAPIWithAspNet-v11/RestfulAPIWithAspNet/Utils/Converter/Parser.cs
+++ RestfulAPIWithAspNet-v11/RestfulAPIWithAspNet/Utils/Converter/Parser.cs
@@ -4,36 +4,4 @@
     {
         D Parse(O origin);
     }
-
-    public abstract class Parser<R>
-    {
-        public Error _Error { get; private set; }
-        public R _Data { get; private set; }
-
-        public bool IsOk
-        {
-            get
-            {
-                return _Error == null;
-            }
-        }
-
-        public bool HasError
-        {
-            get
-            {
-                return !IsOk;
-            }
-        }
-
-        protected Parser(R data)
-        {
-            _Data = data;
-        }
-
-        protected Parser(Error error)
-        {
-            _Error = error;
-        }
-    }
 }
